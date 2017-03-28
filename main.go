731b0ee74f9b8011ef6eb0dbb52fc35ee41085ce
shiimaxx <yoshima@shiimaxx.com>
2017-03-28 20:01:30 +0900

    Remove unused packages

--- main.go
+++ main.go
@@ -8,10 +8,7 @@ import (
 	"os"
 	"strings"
 
-	_ "github.com/urfave/cli"
-
 	"github.com/aws/aws-sdk-go/aws"
-	_ "github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
 )
